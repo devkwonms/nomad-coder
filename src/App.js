75086cdc3126b229737e707783fa4f4bbdcd5850
devkwonms <alstj1379@nate.com>
2023-02-13 21:12:33 +0900

    Movie App part One

--- src/App.js
+++ src/App.js
@@ -1,44 +1,36 @@
 import {useEffect, useState} from "react";
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins,setCoins] = useState([]); 
-  const [cost,setCost] = useState(1);
-  const [need, setNeed] = useState(1);
-  const onChange = (event) => {
-    setCost(event.target.value);
-    setNeed(1);
-  }
-  const handleInput = (event) => {
-    setNeed(event.target.value);
-  } 
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() =>{
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    )
+  ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(()=>{
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  },[]);
-  return (
-  <div>
-    <h1>The coins!{loading ? "" :`Here are..${coins.length} coins`}</h1>
-    {loading ? <strong>loading...</strong> : <select onChange={onChange}>
-      <option>Select Coin!</option>
-      {coins.map((coin , index) =>
-      <option 
-        key={index} 
-        value={coin.quotes.USD.price} 
-        id={coin.symbol}
-        symbol = {coin.symbol} >
-        {coin.name}({coin.symbol}) : ${Math.round(coin.quotes.USD.price)} USD
-        </option>
-    )}
-    </select>}
-    <h2>Please enter the amount</h2>
-    <div>
-      <input type="number" value={need} onChange={handleInput} placeholder="dollor"/>$
+    getMovies();
+  },[])
+  return <div> {loading ? 
+  <h1>Loading...</h1> 
+  : <div>
+    {movies.map(movie => (
+    <div key={movie.id}>
+      <img src={movie.medium_cover_image} />
+      <h2>{movie.title}</h2>
+      <p>{movie.summary}</p>
+      <ul>
+          {movie.genres.map((g) => (
+          <li key={g}>{g}</li>
+          ))}
+      </ul>
     </div>
-    <h2>You can get {Math.round(need/cost)}</h2>
-  </div>);
+    ))}
+    </div>
+    }
+    </div>;
 }
 export default App;

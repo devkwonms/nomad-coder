764f337bc0542ac7686a9c548d750423e95e36f9
devkwonms <alstj1379@nate.com>
2023-02-13 17:48:43 +0900

    Coin Tracker(Api호출,fetch)

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 function App() {
-  const[toDo, setToDo] = useState("");
-  const[toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    
-    if(toDo ===""){
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
-  console.log(toDo);
-  console.log(toDos);
-  return <div>
-    <h1>My To Dos ({toDos.length})</h1>
-    <form onSubmit={onSubmit}>
-      <input 
-        onChange = {onChange} 
-        value = {toDo} 
-        type="Text" 
-        placeholder="Write your to do..."
-      />
-      <button>Add To Do</button>
-    </form>
-    <hr/>
-    <ul>
-    {toDos.map((item, index) => <li key={index}>{item}</li>)}
-    </ul>
-  </div>;
+  const [loading, setLoading] = useState(true);
+  const [coins,setCoins] = useState([]); 
+  const [cost,setCost] = useState(1);
+  const [need, setNeed] = useState(1);
+  const onChange = (event) => {
+    setCost(event.target.value);
+    setNeed(1);
+  }
+  const handleInput = (event) => {
+    setNeed(event.target.value);
+  } 
+  useEffect(()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  },[]);
+  return (
+  <div>
+    <h1>The coins!{loading ? "" :`Here are..${coins.length} coins`}</h1>
+    {loading ? <strong>loading...</strong> : <select onChange={onChange}>
+      <option>Select Coin!</option>
+      {coins.map((coin , index) =>
+      <option 
+        key={index} 
+        value={coin.quotes.USD.price} 
+        id={coin.symbol}
+        symbol = {coin.symbol} >
+        {coin.name}({coin.symbol}) : ${Math.round(coin.quotes.USD.price)} USD
+        </option>
+    )}
+    </select>}
+    <h2>Please enter the amount</h2>
+    <div>
+      <input type="number" value={need} onChange={handleInput} placeholder="dollor"/>$
+    </div>
+    <h2>You can get {Math.round(need/cost)}</h2>
+  </div>);
 }
-
 export default App;

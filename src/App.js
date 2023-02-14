aca2fb6a9740c5a0e87f9d034f5b7adc15373937
devkwonms <alstj1379@nate.com>
2023-02-14 16:23:50 +0900

    Parameters & GHpage setting

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 return <Router>
 <Routes>
 <Route path="/" element={<Home />} />
-<Route path="/movie" element={<Detail />} />
+<Route path="/movie/:id" element={<Detail />} />
 </Routes>
 </Router>;
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
 import {Link}      from "react-router-dom";
-function Movie({coverImg,title,summary,genres}){
+function Movie({id, coverImg,title,summary,genres}){
     return <div>
     <img src={coverImg} alt={title}/>
     <h2>
-        <Link to ="/movie">{title}</Link>
+        <Link to ={`/movie/${id}`}>{title}</Link>
     </h2>
     <p>{summary}</p>
     <ul>
@@ -16,6 +16,7 @@ function Movie({coverImg,title,summary,genres}){
 }
 
 Movie.propTypes = {
+    id: PropTypes.number.isRequired,
     coverImg: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,

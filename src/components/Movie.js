fa2cc0589057382ddbc94c830537886a6e654e39
devkwonms <alstj1379@nate.com>
2023-02-15 18:29:48 +0900

    css 추가

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
 import {Link}      from "react-router-dom";
+import styles from "../components"
 function Movie({id, coverImg,title,summary,genres}){
     return <div>
     <img src={coverImg} alt={title}/>
     <h2>
         <Link to ={`/movie/${id}`}>{title}</Link>
     </h2>
-    <p>{summary}</p>
+    <p>{summary.length > 235? `${summary.slice(0,235)}...`: summary}</p>
     <ul>
         {genres.map((g) => (
         <li key={g}>{g}</li>
